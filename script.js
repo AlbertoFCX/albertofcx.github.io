04d3c79882f7ca132704e365557cfe38e9d93192
OpenShift guest <kometafcx@gmail.com>
2014-02-15 13:38:32 -0500

    Intento javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+$("#slider > div:gt(0)").hide();
+setInterval (function() {
+    $('#slider > div first')
+    .fadeOut(1000)
+    .next
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slider');
+}, 3000);
